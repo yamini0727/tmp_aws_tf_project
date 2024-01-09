e4744e175bafbb782d679003102b34e64e31252c
yamini0727 <81848316+yamini0727@users.noreply.github.com>
2024-01-09 12:52:23 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAYUN4HSW4EUC2PZE7"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
